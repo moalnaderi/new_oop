0020e63850661d90e19e8acf4b2772de778f24a4
MohammadAli Naderi <155289282+moalnaderi@users.noreply.github.com>
2024-06-29 16:02:52 +0330

    Add files via upload

---
+++ src/main/resources/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        InputOutput inputoutput = new InputOutput(scanner);
+        inputoutput.start();
+    }
+}
